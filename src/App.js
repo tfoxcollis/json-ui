70c380abe3a09874b6314413e2d2acd646a64dcd
Trish Fox-Collis <tfoxcollis@gmail.com>
2023-01-15 11:24:14 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [txtBlob, setTxtBlob] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const files = e.target.elements.jsFile.files
+    const fData = new FormData()
+    fData.append('jsonFile', files[0])
+
+    fetch('http://localhost:3001/converter', {
+      method: "POST",
+      mode: "cors",
+      body: files[0]
+    })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data)
+      const file = new Blob([data?.file], { type: 'text/plain' })
+      let downloadTxt = document.getElementById('downloadTxt')
+      downloadTxt.download = data.jobName.split(' ').join('') + '.txt'
+      downloadTxt.href = window.URL.createObjectURL(file)
+      downloadTxt.style.display = 'block'
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="main-box">
+    <h1>Chandelle's JSON converter v1.0</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          file:
+          <input type="file" id="jsFile"/>
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
+        <a id="downloadTxt" style={{display: 'none'}}>
+          Download Document
         </a>
-      </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
